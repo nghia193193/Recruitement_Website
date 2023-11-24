fd4a5b416aedb7146d09b24d929b7d24fdf4c9a7
nghia193193 <nguyennghia193913@gmail.com>
2023-11-24 23:19:07 +0700

    update

--- Recruitment_Website/api/v1/routes/candidate.route.js
+++ Recruitment_Website/api/v1/routes/candidate.route.js
@@ -65,6 +65,31 @@ router.get('/jobs/applicants', middleware_1.isAuth, [
     })
 ], candidateController.GetAppliedJobs);
 router.put('/information', middleware_1.isAuth, [], candidateController.saveInformation);
-router.get('/information', middleware_1.isAuth, [], candidateController.getInformation);
-router.get('/interviews', middleware_1.isAuth, [], candidateController.getAllInterviews);
+router.get('/information', middleware_1.isAuth, candidateController.getInformation);
+router.get('/interviews', middleware_1.isAuth, [
+    (0, express_validator_1.query)('page').trim()
+        .custom((value, { req }) => {
+        if (value) {
+            const regex = /^[0-9]+$/; // Chỉ cho phép số
+            if (!regex.test(value)) {
+                throw new Error('page không hợp lệ');
+            }
+            ;
+            return true;
+        }
+        return true;
+    }),
+    (0, express_validator_1.query)('limit').trim()
+        .custom((value, { req }) => {
+        if (value) {
+            const regex = /^[0-9]+$/; // Chỉ cho phép số
+            if (!regex.test(value)) {
+                throw new Error('limit không hợp lệ');
+            }
+            ;
+            return true;
+        }
+        return true;
+    })
+], candidateController.getAllInterviews);
 exports.default = router;
